eceb3a69023b376a8e8d3c98ce97802727c05fc0
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-01-25 14:21:01 -0500

    Linux installation added

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Make sure to run this command with sudo.
+mkdir /usr/share/pomodoro-cli
+cp -r target/release/pomodoro_cli /usr/share/pomodoro-cli
+cp -r Audio/ /usr/share/pomodoro-cli
+
+chmod +x pomodoro_cli
+cp pomodoro_cli /usr/bin/
