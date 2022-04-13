e56cb23081d56e7143a180cf005f645947db0035
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-04-13 16:38:39 -0500

    Some dependecies stuff added

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 # Make sure to install Rust
 cargo build --release
 # Make sure to run this command with sudo.
-mkdir /usr/share/pomodoro-cli
-cp -r target/release/pomodoro_cli /usr/share/pomodoro-cli
-cp -r Audio/ /usr/share/pomodoro-cli
+sudo mkdir /usr/share/pomodoro-cli
+sudo cp -r target/release/pomodoro_cli /usr/share/pomodoro-cli
+sudo cp -r Audio/ /usr/share/pomodoro-cli
 
 chmod +x pomodoro_cli
-cp pomodoro_cli /usr/bin/
+sudo cp pomodoro_cli /usr/bin/
